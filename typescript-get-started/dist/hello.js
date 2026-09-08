efe6c932b7f1b841ff41b9f8d75c42ee5527b848
shrivatsashetty <shrivatsa307@gmail.com>
2026-09-08 23:43:20 +0530

    refactored

--- typescript-get-started/dist/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-const message = greet("World");
-console.log(message);
-//# sourceMappingURL=hello.js.map
